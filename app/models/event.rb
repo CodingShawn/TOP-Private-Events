31759e7916105c2e974a7393f340bfa1d3cf10a2
Shawn <codingshawn@gmail.com>
2020-08-21 22:15:25 +0800

    Modified Event index page to show events seperated by past and upcoming events

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,4 +3,8 @@ class Event < ApplicationRecord
  
   has_many :event_attendings, foreign_key: :attendee_event_id
   has_many :attendees, through: :event_attendings, source: :event_attendee
+  
+  scope :past_events, -> {where("date < ?", Time.now)}
+  scope :future_events, -> {where("date > ?", Time.now)}
+
 end
